769f9f7ce8cab945b7ec5db9312aff6f2bdf8e4a
Oliver Huckfeldt <oliverhuckfeldt@gmail.com>
2025-01-20 16:55:54 +0100

    Add font family. Create banner section.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      'sans': ['Open Sans', 'sans-serif']
+    },
+    colors: {
+      'primary': '#f44336',
+      'background': '#ffffff'
+    },
     extend: {},
   },
   plugins: [],
